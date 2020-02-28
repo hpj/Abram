bbb622e3e0188e42f897fa18a24e8975871fdb6f
Kerolos Zaki <kerolos4zaki@gmail.com>
2020-02-28 19:30:05 +0200

    chat input field

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView, TextInput } from 'react-native';
+
+import { sizes, screen } from '../sizes.js';
 
 import getTheme from '../colors.js';
 
@@ -12,6 +14,24 @@ class Chat extends React.Component
   {
     return (
       <View style={ styles.container }>
+        <ScrollView style={ styles.messages }>
+
+          <View style={ { ...styles.message, backgroundColor: 'green' } }>
+
+          </View>
+
+          <View style={ { ...styles.message, backgroundColor: 'red' } }>
+
+          </View>
+
+        </ScrollView>
+
+        <View style={ styles.input }>
+          <TextInput style={ styles.field }
+            placeholderTextColor={ colors.placeholder }
+            placeholder={ 'Write Message' }
+          />
+        </View>
 
       </View>
     );
@@ -20,8 +40,42 @@ class Chat extends React.Component
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: colors.whiteBackground
+    backgroundColor: 'orange',
+    height: screen.height - (sizes.topBarHeight + sizes.topBarBigMargin)
+  },
+
+  messages: {
+
+  },
+
+  message: {
+    height: 500
+  },
+
+  input: {
+    backgroundColor: 'purple',
+
+    // backgroundColor: colors.messageBubble,
+    width: screen.width - (sizes.windowMargin * 2),
+
+    marginTop: 10,
+    marginBottom: 10,
+    
+    marginLeft: sizes.windowMargin,
+    marginRight: sizes.windowMargin,
+    
+    borderRadius: sizes.topBarHeight
+  },
+
+  field: {
+    color: colors.whiteText,
+
+    height: sizes.topBarHeight,
+
+    fontSize: 16,
+
+    marginLeft: sizes.windowMargin,
+    marginRight: sizes.windowMargin
   }
 });
 
