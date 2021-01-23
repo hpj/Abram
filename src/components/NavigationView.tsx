42f5beb49a20e3ce7916fb5f21afa85252e5002c
Kerolos Zaki <52022280+ker0olos@users.noreply.github.com>
2021-01-23 09:26:50 +0200

    added a subtle transition between animation pages

--- src/components/NavigationView.tsx
+++ src/components/NavigationView.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import { View } from 'react-native';
+import Animated, { Easing } from 'react-native-reanimated';
 
 import { StoreComponent } from '../store';
 
@@ -13,6 +13,15 @@ class NavigationView extends StoreComponent<{
   index: number
 }>
 {
+  constructor(props: NavigationView['props'])
+  {
+    super(props);
+   
+    this.progress = new Animated.Value(props.index === 0 ? 1 : 0);
+  }
+
+  progress: Animated.Value<number>;
+
   stateWhitelist(changes: NavigationView['state']): boolean
   {
     if (changes.index)
@@ -21,24 +30,60 @@ class NavigationView extends StoreComponent<{
     return false;
   }
 
+  stateDidChange(state: NavigationView['state'], changes: NavigationView['state'], old: NavigationView['state']): void
+  {
+    const { index } = this.props;
+
+    // start show page animation
+    if (state.index === index && state.index !== old.index)
+    {
+      Animated.timing(this.progress, {
+        duration: 125,
+        toValue: 1,
+        easing: Easing.circle
+      }).start();
+    }
+    // start hide page animation
+    else if (old.index === index && state.index !== old.index)
+    {
+      Animated.timing(this.progress, {
+        duration: 125,
+        toValue: 0,
+        easing: Easing.circle
+      }).start();
+    }
+  }
+
   render(): JSX.Element
   {
     const { index, testID } = this.props;
 
     const active = this.state.index === index;
 
-    return <View
+    const opacity = this.progress.interpolate({
+      inputRange: [ 0, 1 ],
+      outputRange: [ 0, 1 ]
+    });
+
+    const top = this.progress.interpolate({
+      inputRange: [ 0, 1 ],
+      outputRange: [ 25, 0 ]
+    });
+
+    return <Animated.View
       testID={ testID }
       style={ {
         position: 'absolute',
         width: '100%',
         height: '100%',
-        opacity: active ? 1 : 0
+        top,
+        opacity
+        // opacity: active ? 1 : 0
       } }
       pointerEvents={ active ? 'box-none' : 'none' }
     >
       { this.props.children }
-    </View>;
+    </Animated.View>;
   }
 }
 
